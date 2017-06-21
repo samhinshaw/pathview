c55cb752479cc703f344854242a3b934491e1780
w.luo <w.luo@bc3139a8-67e5-0310-9ffc-ced21a209358>
2017-06-21 21:09:45 +0000

    new version 1.17.1
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/pathview@130599 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/kegg.species.code.R
+++ R/kegg.species.code.R
@@ -3,7 +3,7 @@ function(species="hsa", na.rm=FALSE, code.only=TRUE){
       nspec=length(species)
       if(!exists("korg")) data(korg)
 
-      ridx=match(species, korg[,1:3]) %% nrow(korg)
+      ridx=match(species, korg[,1:5]) %% nrow(korg)
       nai=is.na(ridx)
       if(sum(nai)>0) {
         na.msg=sprintf("Unknown species '%s'!", paste(species[nai], sep="", collapse="', '"))
@@ -15,7 +15,7 @@ function(species="hsa", na.rm=FALSE, code.only=TRUE){
         }
       if(any(ridx[!nai]==0)) ridx[!nai & ridx==0]=nrow(korg)
       if(na.rm) ridx=ridx[!nai]
-      if(code.only) coln=1 else coln=c(1,4:6)
+      if(code.only) coln=3 else coln=c(3,6:10)
       species.info=korg[ridx,coln]
       return(species.info)
     }
